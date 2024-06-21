c2178340852db2a1666ab6931e80018cdb98d78a
Thomas Lohri <T-Lohri@gmx.de>
2024-06-21 17:10:27 +0200

    Re-Setup Project to use Individual Accounts

--- ChoirManagement/Controllers/HomeController.cs
+++ ChoirManagement/Controllers/HomeController.cs
@@ -23,11 +23,6 @@ namespace ChoirManagement.Controllers
             return View();
         }
 
-        public IActionResult Choir()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
